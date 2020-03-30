91be5ecd025b4434922d79118c1a95c9288d7b33
Esdras <esdraspiano@gmail.com>
2020-03-30 15:55:48 -0300

    Migração para o Jekyll Framework.

---
+++ jekyll/assets/js/mapa.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function() {
+
+	/*$("#mineiros_mapa_jatai").mouseover(function() {
+		console.log("Passou aqui");
+	  $(this).css( "fill", "white" );
+	});*/
+
+	var a = document.getElementById("mapa-jatai").embeddedDom.contentDocument;
+    console.log(a);    
+});
