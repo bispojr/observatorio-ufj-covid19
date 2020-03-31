3d2db4098f68f0340b1e553a3492287c73bcbe1e
Esdras <esdraspiano@gmail.com>
2020-03-31 08:38:41 -0300

    Remoção de prints.

--- jekyll/assets/js/graficos.js
+++ jekyll/assets/js/graficos.js
@@ -42,9 +42,6 @@ function drawJatai() {
 
       var ticksY = [];
       var stepY = Math.ceil((maxY-minY)/5);
-      console.log(minY);
-      console.log(minX);
-      console.log(stepY);
 
       var stepGold = [1, 5, 10, 20, 50, 100, 200, 500];
       for(var i=0; i<stepGold.length; i++){
