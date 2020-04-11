8bf00ee26ab6c333713ea80b474bb4f2ec74f3c0
Esdras <esdraspiano@gmail.com>
2020-04-11 08:41:39 -0300

    Criação da página Tendências

--- jekyll/assets/js/tendencias/buildProjection.js
+++ jekyll/assets/js/tendencias/buildProjection.js
@@ -15,32 +15,48 @@ function buildProjection(param) {
 
   var url = "https://covidgoias.ufg.br/service/indicators/projections?cd_geocmu=" + param.cd_geocmu + "&lang=pt-br";
 
-  $.ajax({
-    type: 'GET',
-    url: url,
-    contentType: 'application/json',
-    headers: {
-      'Access-Control-Allow-Credentials' : true,
-      'Access-Control-Allow-Origin':'https://www.deolhonocorona.com',
-      'Access-Control-Allow-Methods':'GET',
-      'Access-Control-Allow-Headers':'application/json'
-    },
-    success: function(data) {
-      console.log(data);
-    },
-    error: function(error) {
-      console.log("FAIL....=================");
-    }
-  });
-  /*$.getJSON(url, function(dataJson) {
-    
-    console.log(dataJson);
+  $.getJSON(url, function(dataJson) {
 
-    //Build a data table from dataJson
-    //  var data = response.getDataTable();
+    var diaMes = dataJson.timeseries.chartResult[0].dataResult.labels;
+    var confirmados = dataJson.timeseries.chartResult[0].dataResult.datasets[0].data;
+    var projetados = dataJson.timeseries.chartResult[0].dataResult.datasets[1].data;
+
+    //Parser na criação da data
+    var dataUltimoModelo = dataJson.timeseries.chartResult[0].last_model_date;
+    dataUltimoModelo = dataUltimoModelo.split("T")[0].split("-");
+    dataUltimoModelo = new Date(dataUltimoModelo[0], dataUltimoModelo[1] - 1, dataUltimoModelo[2]);
+
+    var data = new google.visualization.DataTable();
+    data.addColumn('date', 'Dia/Mês');
+    data.addColumn('number', 'Projetados');
+    data.addColumn('number', 'Confirmados');
+
+    data.addRows(projetados.length);
+
+    for (var i = 0; i < confirmados.length; i++) {
+      
+      var dateVal = diaMes[i].split("-");
+      dateVal = new Date(dateVal[2], dateVal[1]-1, dateVal[0]);
+      
+      data.setCell(i, 0, dateVal);
+      data.setCell(i, 2, confirmados[i]);
+    }
+
+    //Ligar os gráficos
+    data.setCell(confirmados.length-1, 0, dateVal);
+    data.setCell(confirmados.length-1, 1, confirmados[confirmados.length-1]);
+
+    for (var i = confirmados.length; i < projetados.length; i++) {
+      
+      var dateVal = diaMes[i].split("-");
+      dateVal = new Date(dateVal[2], dateVal[1]-1, dateVal[0]);
+      
+      data.setCell(i, 0, dateVal);
+      data.setCell(i, 1, projetados[i]);
+    }
 
     //Criação do ticks X
-    /*var minX = data.getColumnRange(0).min;
+    var minX = data.getColumnRange(0).min;
     var maxX = data.getColumnRange(0).max;
 
     //Converte em dias
@@ -98,6 +114,13 @@ function buildProjection(param) {
         isStacked: true,
         colors: param.colors,
         pointSize: 5,
+        animation:{
+          duration: 3000,
+          startup: true,
+        },
+        series: {
+          0: { lineDashStyle: [4, 4], pointShape: { type: 'square' }, pointSize: 10 }
+        },
         hAxis: {
             title: param.xTitle,
             format: 'd/M',
@@ -117,7 +140,6 @@ function buildProjection(param) {
       var chart = new google.visualization.LineChart(document.getElementById(param.idDiv));
       chart.draw(data, options);
 
-
       //Data de atualização informada em cada página
       if(param.data_atualizacao != false){
           var mes = [
@@ -126,9 +148,14 @@ function buildProjection(param) {
             "julho", "agosto", "setembro", 
             "outubro", "novembro", "dezembro"];
 
-          $(param.data_atualizacao).text(maxX.getDate() + " de " + mes[maxX.getMonth()] + " de " + maxX.getFullYear());
+          $(param.data_atualizacao).text(
+            dataUltimoModelo.getDate() + " de " + 
+            mes[dataUltimoModelo.getMonth()] + " de " + 
+            dataUltimoModelo.getFullYear()
+          );
       }
-  });*/
+
+  });
 }
 
 function getProjectionParameters(cidade){
@@ -147,16 +174,11 @@ function getJataiParameters(){
   
   var parametros = {};
   
-  parametros.query = 'SELECT A, B, D, E, F, G';
   parametros.colors = [
-    corGrafico.confirmados, corGrafico.investigados, corGrafico.notificados, 
-    corGrafico.isolados, corGrafico.internados];
-  parametros.googleSheet = 'https://docs.google.com/spreadsheets/d/'
-  parametros.googleSheet += '1nCDjAvdEWVzwJjLhRkkVHiw2SK63SKcYXb7doIUI5VQ';
-  parametros.googleSheet += '/gviz/tq?sheet=Dados&headers=1&tq=';
+    "royalblue", corGrafico.confirmados];
   parametros.xTitle = 'Dia/Mês';
   parametros.yTitle = 'Número de casos';
-  parametros.idDiv = 'jatai-grafico-resumo';
+  parametros.idDiv = 'jatai-tendencias';
   parametros.data_atualizacao = "#data-atualizacao-jatai";
   parametros.cd_geocmu = "5211909";
 
@@ -165,20 +187,15 @@ function getJataiParameters(){
 }
 
 function getRioVerdeParameters(){
-/*
-  B = Confirmados,  C = Descartados 
-  D = Investigados  E= Notificados 
-  F = Isolados      G = Internados  
-  H = Monitorados   I = Recuperados
-*/
 
   var parametros ={};
 
-  parametros.googleSheet = 'https://docs.google.com/spreadsheets/d/'
-  parametros.googleSheet += '1jrhI1EjA8KJNJ5CKEDe-oREPjeRnYviVKp9AJPPMlLE';
-  parametros.googleSheet += '/gviz/tq?sheet=Dados&headers=1&tq=';
+  parametros.colors = [
+    "royalblue", corGrafico.confirmados];
   parametros.xTitle = 'Dia/Mês';
   parametros.yTitle = 'Número de casos';
+  parametros.idDiv = 'rioverde-tendencias';
+  parametros.data_atualizacao = "#data-atualizacao-rioverde";
   parametros.cd_geocmu = "5218805";
 
   return parametros;
