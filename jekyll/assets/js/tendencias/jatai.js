d7aa256d4fb3d55fafe0182b50199643c63d8b61
Esdras <esdraspiano@gmail.com>
2020-04-17 17:41:28 -0300

    Merge branch "feature-30" into dev

---
+++ jekyll/assets/js/tendencias/jatai.js
@@ -0,0 +1,7 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawProjection);
+
+function drawProjection() {
+
+  buildProjection(getProjectionParameters("jatai"));
+}
