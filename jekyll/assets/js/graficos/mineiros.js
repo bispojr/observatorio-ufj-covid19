a691e5c44bb68f272dd70a6f4d4a3aef9266c677
Esdras <esdraspiano@gmail.com>
2020-04-21 16:25:42 -0300

    Atualização dos cards e do gráfico Mineiros.

--- jekyll/assets/js/graficos/mineiros.js
+++ jekyll/assets/js/graficos/mineiros.js
@@ -8,11 +8,11 @@ function drawChart() {
   parametros.mineiros ={};
 
   parametros.mineiros.resumo = {};
-  parametros.mineiros.resumo.query = 'SELECT A, B, C, D';
+  parametros.mineiros.resumo.query = 'SELECT A, E, H, I, G';
   parametros.mineiros.resumo.googleSheet = 'https://docs.google.com/spreadsheets/d/'
   parametros.mineiros.resumo.googleSheet += '1MPFPI6nZvoPjSanXFcVqh3TrWDRJ7J5SXbhKkW6p5NY';
   parametros.mineiros.resumo.googleSheet += '/gviz/tq?sheet=Dados&headers=1&tq=';
-  parametros.mineiros.resumo.colors = ['green', 'brown', 'orange'];
+  parametros.mineiros.resumo.colors = ["red", 'purple', 'blue', 'black'];
   parametros.mineiros.resumo.xTitle = 'Dia/Mês';
   parametros.mineiros.resumo.yTitle = 'Número de casos';
   parametros.mineiros.resumo.idDiv = 'mineiros-grafico-resumo';
