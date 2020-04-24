69458150f595579765f3f6c8daf83ede7015454f
diego costa <diegocosta@diegocostadev.local>
2020-04-24 13:29:45 -0300

    Alterando configuracao staticfiles

--- observatorio/settings.py
+++ observatorio/settings.py
@@ -115,8 +115,8 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, '/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static/"),
 )
