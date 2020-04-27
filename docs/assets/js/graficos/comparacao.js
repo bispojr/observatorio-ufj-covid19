1bf325ecfc1c9d1cfecc396375b71c3d4f0263f8
Esdras <esdraspiano@gmail.com>
2020-04-27 18:03:11 -0300

    Build v2.6.0

--- docs/assets/js/graficos/comparacao.js
+++ docs/assets/js/graficos/comparacao.js
@@ -1,4 +1,4 @@
-google.charts.load('current', {'packages':['corechart']});
+google.charts.load('current', {'packages':['corechart'], 'language': 'pt'});
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
