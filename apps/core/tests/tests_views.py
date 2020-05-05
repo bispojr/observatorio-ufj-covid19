8c64699307a2c7dec4c2ff4cde8e0240dea6843c
Esdras <esdraspiano@gmail.com>
2020-05-05 13:01:11 -0300

    Corrigindo o teste criado

--- apps/core/tests/tests_views.py
+++ apps/core/tests/tests_views.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.test import Client
 
 # Create your tests here.
 
 class AcessosViewTestCase(TestCase):
 
     def test_status_code_200(self):
-        response = self.client.get(reverse("home"))
+        c = Client()
+        response = c.post('/')
         self.assertEquals(response.status_code, 200)
