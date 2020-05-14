2a1ff7eadcb3c06f8901932bd472a772ea81f3b1
Esdras <esdraspiano@gmail.com>
2020-05-14 18:49:48 -0300

    Voltando ao uso do webdriver

--- apps/core/tests/tests_views.py
+++ apps/core/tests/tests_views.py
@@ -11,7 +11,7 @@ class AcessosViewTestCase(TestCase):
 
     def test_home_acesso(self):
 
-        driver = webdriver.Firefox()
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
         
         driver.get('http://127.0.0.1:8000/')
         assert "Observatório UFJ Covid-19 - Principal" in driver.title
