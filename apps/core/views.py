2310d79143adbda22ceda335bd6d4136dd52f7ca
Felipe Nedopetalski <37910959+fNedopetalski@users.noreply.github.com>
2020-05-15 13:52:59 -0300

    Corrigindo erro

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from django.views.decorators.http import require_GET
 
+from apps.core.api import (quantidade_estado_goias, quantidade_geral_brasil, 
+			api_brasil_estado, api_brasil_geral)
 from .models import Graficos
 from .models import Comparacao
 from .models import Home
