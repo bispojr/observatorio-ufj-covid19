5d5f58afe5801ffc5e055c35ddb55ba826a2a1cf
fNedopetalski <fnedopetalski@gmail.com>
2020-05-19 16:08:08 -0300

    Teste com chrome

--- apps/core/tests/tests__fails.py
+++ apps/core/tests/tests__fails.py
@@ -1,26 +1,28 @@
 from django.test import TestCase
 from django.test import Client
 
-from selenium import webdriver
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options
+# from selenium import webdriver
+# from webdriver_manager.firefox import GeckoDriverManager
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.firefox.options import Options
+from selenium.webdriver import Chrome
 
 # Create your tests here.
 
 class FailsTestCase(TestCase):
     
     def setUp(self):
-        options = Options()
-        options.headless = True
-        self.driver = webdriver.Firefox(options=options, executable_path=GeckoDriverManager().install())
+        # options = Options()
+        # options.headless = True
+        self.driver = Chrome() # executable_path=GeckoDriverManager().install()
 
     def tearDown(self):
         self.driver.close()
-
-    """ def test_menu_ativo(self):
+    
+    #Teste com chrome retornando OK deste modo
+    def test_menu_ativo(self):
         self.driver.get('http://127.0.0.1:8000/graficos/jatai')
-        elements = self.driver.find_element_by_xpath('//div[@id="navbarDropdownGraficos" and @class="active"]')
-        assert len(elements) > 0 """
+        elements = self.driver.find_elements_by_id("navbarDropdownGraficos")
+        assert len(elements) > 0 
 
     
