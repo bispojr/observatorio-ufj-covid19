6ecec21053c0da2d2e38dc24643028486256bfd3
Esdras <esdraspiano@gmail.com>
2020-05-21 21:08:39 -0300

    Restauração dos imports do tests__fails

--- apps/core/tests/tests__fails.py
+++ apps/core/tests/tests__fails.py
@@ -1,11 +1,10 @@
 from django.test import TestCase
 from django.test import Client
 
-# from selenium import webdriver
-# from webdriver_manager.firefox import GeckoDriverManager
-# from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver import Chrome
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
 
 # Create your tests here.
 
@@ -14,17 +13,10 @@ class FailsTestCase(TestCase):
     def setUp(self):
         options = Options()
         options.headless = True
-        self.driver = Chrome(options=options) # executable_path=GeckoDriverManager().install()
+        self.driver = webdriver.Firefox(options=options, executable_path=GeckoDriverManager().install())
 
     def tearDown(self):
         self.driver.close()
-    
-    #Teste com chrome retornando OK deste modo
-    def test_menu_ativo(self):
-        self.driver.get('http://127.0.0.1:8000/graficos/jatai')
-        elements = self.driver.find_element_by_id("navbarDropdownGraficos")
-        is_active = "active" in elements.get_attribute("class")
-        assert is_active == True
         
 
     
