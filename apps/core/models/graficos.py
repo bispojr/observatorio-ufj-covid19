80903acea3cc9c40a31989cfc22af3f01ef9948c
Esdras <esdraspiano@gmail.com>
2020-05-25 12:14:31 -0300

    Atualização dos cards.

--- apps/core/models/graficos.py
+++ apps/core/models/graficos.py
@@ -98,9 +98,9 @@ class Graficos(models.Model):
                 "nome_base": "mineiros",
                 "url_fonte": "http://mineiros.go.gov.br/covid-19.php",
                 "nome_fonte": "Secretaria de Saúde de Mineiros",
-                "data": "23 de maio"
+                "data": "25 de maio"
             },
-            "querysets": self.__cardDict(22, 16, 0, 0),
+            "querysets": self.__cardDict(23, 16, 0, 0),
             "google_charts": ChartBuilder.getValoresMineiros(ChartBuilder)
         }
 
@@ -116,9 +116,9 @@ class Graficos(models.Model):
                 "nome_base": "rioverde",
                 "url_fonte": "https://www.rioverde.go.gov.br/covid19/",
                 "nome_fonte": "Secretaria de Saúde de Rio Verde",
-                "data": "21 de maio"
+                "data": "24 de maio"
             },
-            "querysets": self.__cardDict(34, 20, 0, 2),
+            "querysets": self.__cardDict(40, 22, 5, 2),
             "google_charts": ChartBuilder.getValoresRioVerde(ChartBuilder)
         }
 
