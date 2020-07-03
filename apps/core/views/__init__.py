9be0d18ad7786fffe7d32cb676808e74225c1bb1
Esdras <esdraspiano@gmail.com>
2020-07-03 12:19:49 -0300

    Refatorações e adaptações.

--- apps/core/views/__init__.py
+++ apps/core/views/__init__.py
@@ -1,3 +1,4 @@
 from .geral import *
 from .graficos import *
 from .saiba_mais import *
+from .boletimEpidemiologico import *
--- apps/core/views/boletimEpidemiologico.py
+++ apps/core/views/boletimEpidemiologico.py
@@ -1,22 +1,10 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.views.decorators.http import require_GET, require_POST, require_http_methods
 
-from apps.core.api import (quantidade_estado_goias, quantidade_geral_brasil, 
-			api_brasil_estado, api_brasil_geral)
-from .models import Graficos
-from .models import Comparacao
-from .models import Home
-from .models import ComoSaoCriados
-from .models import Sobre
-from .models import NaMidia
-from .models import Colabore
-from .models import Tendencias
-from .models import Equipe
-from .models import Noticias
 from apps.core.models import BoletimEpidemiologico
 
-from .forms.createForm import CreateBoletimEpidemiologicoForm
-from .forms.deleteForm import DeleteBoletimEpidemiologicoForm
+from apps.core.forms.createForm import CreateBoletimEpidemiologicoForm
+from apps.core.forms.deleteForm import DeleteBoletimEpidemiologicoForm
 
 import csv 
 from django.conf import settings
@@ -85,7 +73,8 @@ def updateBoletimEpidemiologico(request):
 	context = {'form': form}
 	return render(request, url, context)	
 
-def home(request):
+@require_GET
+def principalBoletimEpidemiologico(request):
 	url = "base.html"
 	context = Home.getContext(Home)
 
@@ -189,75 +178,3 @@ def home(request):
 		
 
 	return render(request, url, context)
-
-
-@require_GET
-def grafico(request, cidade):
-	url = "grafico/cidade.html"
-	context = Graficos.getContext(Graficos, cidade)
-
-	return render(request, url, context)
-
-
-@require_GET
-def comparacao(request):
-	url = "grafico/comparacao.html"
-	context = Comparacao.getContext(Comparacao, request)
-
-	return render(request, url, context)
-
-
-@require_GET
-def como_sao_criados(request):
-	url = "grafico/como-sao-criados.html"
-	context = ComoSaoCriados.getContext(ComoSaoCriados)
-
-	return render(request, url, context)
-
-
-@require_GET
-def equipe(request):
-    context = Equipe.getContext(Equipe)
-    url = "saiba_mais/equipe.html"
-
-    return render(request, url, context)
-
-
-@require_GET
-def sobre(request):
-	url = "saiba_mais/sobre.html"
-	context = Sobre.getContext(Sobre)
-
-	return render(request, url, context)
-
-
-@require_GET
-def na_midia(request):
-	url = "saiba_mais/na-midia.html"
-	context = NaMidia.getContext(NaMidia)
-
-	return render(request, url, context)
-
-
-@require_GET
-def colabore(request):
-	url = "saiba_mais/colabore.html"
-	context = Colabore.getContext(Colabore)
-
-	return render(request, url, context)
-
-
-@require_GET
-def noticias(request):
-	url = "saiba_mais/noticias.html"
-	context = Noticias.getContext(Equipe)
-
-	return render(request, url, context)
-
-
-@require_GET
-def tendencias(request, cidade):
-    informacoes = Tendencias.get_informações(Tendencias, cidade)
-    url = "tendencias/cidade.html"
-
-    return render(request, url, informacoes)
