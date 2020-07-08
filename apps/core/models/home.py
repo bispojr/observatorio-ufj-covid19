f49ed97da76b82efb98d5bdc0a894521f397240c
fNedopetalski <fnedopetalski@gmail.com>
2020-07-08 17:14:26 -0300

    DocStrings dos models do home.py

--- apps/core/models/home.py
+++ apps/core/models/home.py
@@ -1,7 +1,25 @@
 
 class Home():
+    """
+    Classe para criar o context da Home
 
+    methods:
+        getContext
+    """
     def getContext(self):
+         """
+        Retorna o contexto da página contendo:
+            - grupo
+            - grupo_link
+            - titulo
+            - script
+        
+        args:
+            self
+        
+        return:
+            Dict com os dados do contexto da página.
+        """
         return self.__contextHome(self)
 
     def __contextHome(self):
