2bee0deb0ef8406bde88310b27f8d9ee6e6c25b6
fNedopetalski <fnedopetalski@gmail.com>
2020-07-08 17:15:29 -0300

    DocStrings dos models do na_midia.py

--- apps/core/models/na_midia.py
+++ apps/core/models/na_midia.py
@@ -1,7 +1,25 @@
 
 class NaMidia():
+    """
+    Classe para criar o context da página Na Mídia
 
+    methods:
+        getContext
+    """
     def getContext(self):
+        """
+        Retorna o contexto da página contendo:
+            - grupo
+            - grupo_link
+            - script
+            - titulo
+        
+        args:
+            self
+        
+        return:
+            Dict com os dados do contexto da página.
+        """
         return self.__contextNaMidia(self)
     
     def __contextNaMidia(self):
