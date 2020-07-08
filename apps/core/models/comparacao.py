edab2928e8d045917c2710ecfa424e2e0feffa45
fNedopetalski <fnedopetalski@gmail.com>
2020-07-08 17:03:51 -0300

    DocStrings dos models do comparacao.py

--- apps/core/models/comparacao.py
+++ apps/core/models/comparacao.py
@@ -2,8 +2,29 @@ from apps.core.api import (quantidade_estado_goias, quantidade_geral_brasil,
 							api_brasil_estado, api_brasil_geral)
 
 class Comparacao():
+	"""
+    Classe para criar o context da página Comparacao
 
+    methods:
+        getContext
+    """
 	def getContext(self, request):
+		"""
+        Retorna o contexto da página contendo:
+            - grupo
+            - grupo_link
+			- script
+            - titulo
+			- nome_base
+			- goias
+			- brasil
+        
+        args:
+            self
+        
+        return:
+            Dict com os dados do contexto da página.
+        """
 		return self.__contextComparacao(self, request)
 
 	def __contextComparacao(self, request):
