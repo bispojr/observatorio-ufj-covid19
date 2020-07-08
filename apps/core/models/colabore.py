ad49c4506348fa49ddf2b77ed0e2fe34aedb8fd5
fNedopetalski <fnedopetalski@gmail.com>
2020-07-08 16:55:50 -0300

    DocStrings dos models do colabore.py

--- apps/core/models/colabore.py
+++ apps/core/models/colabore.py
@@ -1,7 +1,24 @@
 
 class Colabore():
+    """
+    Classe para criar o context da página Colabore
 
+    methods:
+        getContext
+    """
     def getContext(self):
+        """
+        Retorna o contexto da página contendo:
+            - grupo
+            - grupo_link
+            - titulo
+        
+        args:
+            self
+        
+        return:
+            Dict com os dados do contexto da página.
+        """
         return self.__contextColabore(self)
 
     def __contextColabore(self):
