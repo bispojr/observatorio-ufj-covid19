0c280bb1f52799612645cbcd9f16351e8f12a77a
fNedopetalski <fnedopetalski@gmail.com>
2020-07-09 15:28:29 -0300

    DocStrings api.py

--- apps/core/api.py
+++ apps/core/api.py
@@ -6,6 +6,20 @@ import requests
 
 @require_GET
 def obtem_ultimo_dia(request): 
+	"""
+	Função para pegar a última data 
+	de atualização da API do brasil.io.
+
+	args:
+		- request
+	
+	return:
+		- objeto contendo a data do último dia
+		disponibilizado pela API.
+	
+	requisitos:
+		- datetime
+	"""
 	# a API do brasil.io so exibe os dados do ultimo dia
 	hoje = datetime.date.today() 
 	um_dia = datetime.timedelta(days=1) 
@@ -15,6 +29,20 @@ def obtem_ultimo_dia(request):
 
 @require_GET
 def quantidade_estado_goias(request):
+	"""
+	Função para pegar a quantidade de casos
+	do estado de goiás da API do brasil.io.
+
+	args:
+		- request
+	
+	return:
+		- inteiro com a quantidade de casos
+		do estado de goiás.
+	
+	requisitos:
+		- Função api_brasil_estado do arquivo
+	"""
 	# obtem massa de dados
 	results = api_brasil_estado(request)
 	# cada estado possui um informativo geral
@@ -25,6 +53,20 @@ def quantidade_estado_goias(request):
 
 @require_GET
 def api_brasil_estado(request):
+	"""
+	Função para pegar os dados do último dia
+	do estado de Goiás.
+
+	args:
+		- request
+	
+	return:
+		- JSON contendo os dados do último dia
+		do estado de goiás.
+	
+	requisitos:
+		- Função obtem_ultimo_dia do arquivo.
+	"""
 	data = obtem_ultimo_dia(request)
 
 	payload = {"state": "GO", "date": data, "city": "None"}
@@ -36,6 +78,20 @@ def api_brasil_estado(request):
 
 
 def quantidade_geral_brasil(request):
+	"""
+	Função para obter o subtotal de casos
+	por estado
+
+	args:
+		- request
+
+	return:
+		- lista com o subtotal de casos
+		por estado
+
+	requisitos:
+		- Função api_brasil_geral do arquivo
+	"""
 	# obtem massa de dados
 	results = api_brasil_geral(request)
 
@@ -48,6 +104,20 @@ def quantidade_geral_brasil(request):
 
 @require_GET
 def api_brasil_geral(request):
+	"""
+	Função para obter os dados da API
+	do brasil.io para o Brasil.
+
+	args:
+		- request
+	
+	return:
+		- JSON contendo os dados do Brasil
+		segundo a API do brasil.io
+	
+	requisitos:
+		- Função obtem_ultimo_dia do arquivo.
+	"""
 	data = obtem_ultimo_dia(request)
 
 	payload = {"date": data, "city": "None"}
